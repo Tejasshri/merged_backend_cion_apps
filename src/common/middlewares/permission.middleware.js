ac1e5482222965eff120e72b2023a365422bab82
tejasshri <tejasshrishekhar08@gmail.com>
2024-06-18 16:08:50 +0530

    update

--- src/common/middlewares/permission.middleware.js
+++ src/common/middlewares/permission.middleware.js
@@ -3,9 +3,11 @@ const permissionCheck = async (req, res, next) => {
     const { mongoDB } = req;
     if (!mongoDB) return res.status(401).json({ msg: "DB not connected" });
 
-    const roles = await mongoDB.collection("roles");
-    const permissionGroups = await mongoDB.collection("permissionGroups");
-    console.log(permissionGroups, roles);
+    const rolesCollection = await mongoDB.collection("roles");
+    const permissionGroupsCollection = await mongoDB.collection("permissionGroups");
+    const roles = await rolesCollection.find() ; 
+    const permissionGroups = await permissionGroupsCollection.find() ; 
+    console.log(roles, permissionGroups)
     next();
   } catch (error) {
     res.status(500).json({ msg: "Internal Server Error " + error.message });
