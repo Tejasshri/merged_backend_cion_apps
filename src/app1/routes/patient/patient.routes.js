b3611e1269505328faec109d9a2b485db00044d4
tejasshri <tejasshrishekhar08@gmail.com>
2024-06-18 18:05:48 +0530

    update

--- src/app1/routes/patient/patient.routes.js
+++ src/app1/routes/patient/patient.routes.js
@@ -9,61 +9,66 @@ const permissionCheck = require("../../../common/middlewares/permission.middlewa
 
 const patientRouter = Router();
 
-patientRouter.post("/patient-list", userAuthentication, async (req, res) => {
-  try {
-    const { mongoDB } = req;
-    let { user_number } = req.body;
-    const collection = await mongoDB.collection("patients");
-    const messageCollection = await mongoDB.collection("messages");
-    let data;
+patientRouter.post(
+  "/patient-list",
+  (...param) => permissionCheck(...param, "patient", "read"),
+  userAuthentication,
+  async (req, res) => {
+    try {
+      const { mongoDB } = req;
+      let { user_number } = req.body;
+      const collection = await mongoDB.collection("patients");
+      const messageCollection = await mongoDB.collection("messages");
+      let data;
 
-    // If a specific user number is provided
-    if (user_number) {
-      data = await collection.findOne(
-        { patient_phone_number: user_number },
-        { messages: 1 }
-      );
+      // If a specific user number is provided
+      if (user_number) {
+        data = await collection.findOne(
+          { patient_phone_number: user_number },
+          { messages: 1 }
+        );
 
-      if (!data) {
-        return res.status(404).json({ msg: "User not found", status: 404 });
-      }
-
-      const lastMessageId = data.message_ids[data.message_ids.length - 1];
-      const lastMessage = await messageCollection.findOne(
-        { id: lastMessageId },
-        { projection: { media_data: 0 } }
-      );
-
-      data.lastMessage = lastMessage;
-    } else {
-      // If no specific user number is provided, get all patients
-      data = await collection.find({}, { messages: 1 }).toArray();
-
-      for (let userData of data) {
-        const lastMessageId =
-          userData.message_ids?.[userData.message_ids.length - 1];
-        if (lastMessageId) {
-          const lastMessage = await messageCollection.findOne(
-            { id: lastMessageId },
-            { projection: { media_data: 0 } }
-          );
-          userData.lastMessage = lastMessage;
+        if (!data) {
+          return res.status(404).json({ msg: "User not found", status: 404 });
         }
+
+        const lastMessageId = data.message_ids[data.message_ids.length - 1];
+        const lastMessage = await messageCollection.findOne(
+          { id: lastMessageId },
+          { projection: { media_data: 0 } }
+        );
+
+        data.lastMessage = lastMessage;
+      } else {
+        // If no specific user number is provided, get all patients
+        data = await collection.find({}, { messages: 1 }).toArray();
+
+        for (let userData of data) {
+          const lastMessageId =
+            userData.message_ids?.[userData.message_ids.length - 1];
+          if (lastMessageId) {
+            const lastMessage = await messageCollection.findOne(
+              { id: lastMessageId },
+              { projection: { media_data: 0 } }
+            );
+            userData.lastMessage = lastMessage;
+          }
+        }
+
+        // Filter and sort the data based on the last message timestamp
+        data = data.filter((userData) => userData.lastMessage);
+        data.sort(
+          (i1, i2) => i2.lastMessage.timestamp - i1.lastMessage.timestamp
+        );
       }
 
-      // Filter and sort the data based on the last message timestamp
-      data = data.filter((userData) => userData.lastMessage);
-      data.sort(
-        (i1, i2) => i2.lastMessage.timestamp - i1.lastMessage.timestamp
-      );
+      res.json({ data: data });
+    } catch (error) {
+      console.error(`Error occured in getting patient list`);
+      res.status(500).json({ msg: "Internal Server Error", status: 500 });
     }
-
-    res.json({ data: data });
-  } catch (error) {
-    console.error(`Error occured in getting patient list`);
-    res.status(500).json({ msg: "Internal Server Error", status: 500 });
   }
-});
+);
 
 patientRouter.post(
   "/update-user-note",
